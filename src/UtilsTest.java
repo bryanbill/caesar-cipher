41112581586f87c782d92f180afc67e97172a9a4
bryanbill <emobrya@gmail.com>
2022-04-22 12:49:41 +0300

    fix:Runtime Exception - Catch Runtime Exception

---
+++ src/UtilsTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+class UtilsTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readInt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void print() {
+    }
+}
