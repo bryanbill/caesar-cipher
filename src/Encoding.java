73b79ce02b047bbd0dd6a5d9e9fb56d92a676db4
bryanbill <emobrya@gmail.com>
2022-04-24 10:28:01 +0300

    feat: Encoding -  Encoding Class and Methods

---
+++ src/Encoding.java
@@ -0,0 +1,12 @@
+public class Encoding {
+    private int key;
+    private String payload;
+    public  Encoding(int key, String payload){
+        this.key = key;
+        this.payload = payload;
+    }
+
+    public static String encode(){
+        return "";
+    }
+}
